40b43636830b8f0a9e983fc16d3f13b366002ae5
YL <yl1430834495@163.com>
2020-03-30 17:51:53 +0800

    1.0.5版本发布

--- src/main/java/com/labbol/api/support/response/AbstractQueryAPIResponse.java
+++ src/main/java/com/labbol/api/support/response/AbstractQueryAPIResponse.java
@@ -3,8 +3,21 @@
  */
 package com.labbol.api.support.response;
 
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.Converter;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.reflect.TypeUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.yelong.http.response.HttpResponse;
 
 import com.labbol.api.support.response.support.QueryResult;
@@ -16,13 +29,38 @@ import com.labbol.api.support.response.support.QueryResult;
  */
 public class AbstractQueryAPIResponse<T> extends AbstractAPIResponse implements QueryAPIResponse<T>{
 
-	private QueryResult<T> queryResult;
+	private final QueryResult<T> queryResult;
+
+	private boolean isConvert = false;
+
+	static {
+		//注册Date转换器
+		ConvertUtils.register(new Converter() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public <T> T convert(Class<T> type, Object value) {
+				if( null == value ) {
+					return null;
+				}
+				if( !(value instanceof CharSequence)) {//只能转换字符串类型
+					throw new RuntimeException("只能将字符串类型转换为java.util.Date类型");
+				}
+				try {
+					return (T)DateUtils.parseDate((String)value, "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd");
+				} catch (ParseException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}, Date.class);
+	}
 
 	@SuppressWarnings("unchecked")
 	public AbstractQueryAPIResponse(HttpResponse httpResponse) {
 		super(httpResponse);
 		if(isSuccess()) {
 			queryResult = gson.fromJson(httpResponse.getContentStr(),QueryResult.class);
+		} else {
+			queryResult = null;
 		}
 	}
 
@@ -30,12 +68,59 @@ public class AbstractQueryAPIResponse<T> extends AbstractAPIResponse implements 
 		return queryResult;
 	}
 
+	@Override
+	public QueryResult<T> getQueryResultToBean() {
+		if(!isConvert) {
+			convertMapToBean();
+		}
+		return queryResult;
+	}
+	
 	public Long getTotal() {
 		return queryResult.getTotal();
 	}
 
 	public List<T> getRoot() {
-		return queryResult.getRoot();
+		return getQueryResult().getRoot();
+	}
+	
+	@Override
+	public List<T> getRootToBean() {
+		return getQueryResultToBean().getRoot();
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected void convertMapToBean(){
+		if(!isConvert) {
+			try {
+				Class<?> beanType = getBeanType();
+				//gson无法转换运行时的泛型。这里的List里面为Map类型 咱转换为pojo
+				List<?> sourceRoot = queryResult.getRoot();
+				List<T> root = new ArrayList<T>(sourceRoot.size());
+				for (Object map : sourceRoot) {
+					T bean = (T) beanType.newInstance();
+					BeanUtils.populate(bean, (Map)map);
+					root.add(bean);
+				}
+				queryResult.setRoot(root);
+				isConvert = true;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Class<T> getBeanType(){
+		Map<TypeVariable<?>, Type> typeArguments = TypeUtils.getTypeArguments(getClass(), AbstractQueryAPIResponse.class);
+		if(MapUtils.isNotEmpty(typeArguments)) {
+			for (Entry<TypeVariable<?>, Type> entry : typeArguments.entrySet()) {
+				if(entry.getKey().getName().contentEquals("T")) {
+					return (Class<T>) entry.getValue();
+				}
+			}
+		}
+		throw new RuntimeException("未发现泛型model");
 	}
 
 }
--- src/main/java/com/labbol/api/support/response/QueryAPIResponse.java
+++ src/main/java/com/labbol/api/support/response/QueryAPIResponse.java
@@ -13,11 +13,41 @@ import com.labbol.api.support.response.support.QueryResult;
  * @since 1.0
  */
 public interface QueryAPIResponse<T> extends APIResponse{
-
+	
+	/**
+	 * 获取查询的记录
+	 * @see #getRoot()
+	 * @return
+	 */
 	QueryResult<T> getQueryResult();
 
+	/**
+	 * 对于直接将数据返回给前台的操作请不要调用此方法。map转换为bean时间挺长的
+	 * @see #getRootToBean()
+	 * @since 1.0.5
+	 * @return
+	 */
+	QueryResult<T> getQueryResultToBean();
+	
+	/**
+	 * @return 记录数
+	 */
 	Long getTotal();
 
+	/**
+	 * 获取查询的记录信息。由于gson无法获取运行时泛型所以这个集合中存储的均为Map
+	 * 如果要获取转换为泛型类型的集合请调用 {@link #getRootToBean()}
+	 * 在首次调用 getRootToBean()后该方法将不会返回Map
+	 * @see #getRootToBean
+	 * @return 记录信息
+	 */
 	List<T> getRoot();
 
+	/**
+	 * 获取记录且将map转换为指定的bean对象
+	 * @since 1.0.5
+	 * @return bean集合
+	 */
+	List<T> getRootToBean();
+	
 }
